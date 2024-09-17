317c74168a08e03749ba98229b276f5f4f03d807
Aidan Kaiser <apkaiser@wisc.edu>
2024-09-17 14:43:55 -0700

    Vulnerable bank fix

--- src/VulnerableBank.sol
+++ src/VulnerableBank.sol
@@ -3,40 +3,21 @@ pragma solidity ^0.8.10;
 contract VulnerableBank {
     mapping (address => uint) userBalance;
    
-    function getBalance(address u) constant returns(uint){
+    function getBalance(address u) public view returns(uint){
         return userBalance[u];
     }
 
-    function addToBalance() payable{
+    function addToBalance() public payable {
         userBalance[msg.sender] += msg.value;
     }   
 
-    function withdrawBalance(){
+    function withdrawBalance() public {
         // send userBalance[msg.sender] ethers to msg.sender
-        // if mgs.sender is a contract, it will call its fallback function
-        if( ! (msg.sender.call.value(userBalance[msg.sender])() ) ){
-            throw;
+        // if msg.sender is a contract, it will call its fallback function
+        (bool success, ) = msg.sender.call{value: userBalance[msg.sender]}("");
+        if (!success) {
+            revert();
         }
         userBalance[msg.sender] = 0;
-    }   
-
-    function withdrawBalance_fixed(){
-        // to protect against re-entrancy, the state variable
-        // has to be change before the call
-        uint amount = userBalance[msg.sender];
-        userBalance[msg.sender] = 0;
-        if( ! (msg.sender.call.value(amount)() ) ){
-            throw;
-        }
-    }   
-
-    function withdrawBalance_fixed_2(){
-        // send() and transfer() are safe against reentrancy
-        // they do not transfer the remaining gas
-        // and they give just enough gas to execute few instructions    
-        // in the fallback function (no further call possible)
-        msg.sender.transfer(userBalance[msg.sender]);
-        userBalance[msg.sender] = 0;
-    }   
-   
+    }
 }
